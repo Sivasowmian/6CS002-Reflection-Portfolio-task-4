b9b358cda5f2db8450dbdc8b795e635684af5f92
Balakrishnan Sivasowmian <65554181+Sivasowmian@users.noreply.github.com>
2021-02-09 10:19:26 +0530

    Accessibility and details, print field details.

---
+++ Reflection06.java
@@ -0,0 +1,13 @@
+import java.lang.reflect.Field;
+public class Reflection06 {
+	
+	public static void main(String[] args) throws Exception {
+		Simple s = new Simple();
+		Field [] fields = s.getClass().getDeclaredFields();
+		System.out.printf("There are %d fields\n", fields.length);
+		for(Field f : fields) {
+			System.out.printf("field name=%s type=%s accessible=%s\n", f.getName(), f.getType(), f.isAccessible());
+		}
+	}
+
+}
