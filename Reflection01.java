2fed5121ed335d97c88d257b122567e9a41affa2
Balakrishnan Sivasowmian <65554181+Sivasowmian@users.noreply.github.com>
2021-02-09 10:12:33 +0530

    Reflection: Add reflection01 and reflection02

---
+++ Reflection01.java
@@ -0,0 +1,10 @@
+
+public class Reflection01 {
+	
+	public static void main(String[] args) {
+		Simple new_obj = new Simple();
+		
+		System.out.println("obj_1=" + new_obj);
+	}
+
+}
---
+++ Reflection02.java
@@ -0,0 +1,16 @@
+
+public class Reflection02 {
+	
+	public static void main(String[] args) {
+		Simple obj2 = new Simple();
+		obj2.squareA();
+		
+//		s.squareB();
+		float num1 = obj2.var_a;
+	
+//		double num2 = s.num2;
+		System.out.println("obj2=" + obj2);
+	}
+
+
+}
